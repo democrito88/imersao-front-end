13677ae03170f210b04b526c4f5f61a68bb9fdd5
democrito88 <d_who@hotmail.com>
2025-01-31 00:30:23 -0300

    31-01-2025

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # <img src="./assets/icons/favicon.png" width="20" > Mockify
 
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+[![Licence](https://img.shields.io/github/license/Ileriayo/markdown-badges?style=for-the-badge)](./LICENSE)
+![ClickHouse](https://img.shields.io/badge/v0.1.0-FFCC01?style=for-the-badge)
+
 Projeto de clone da página principal do spotify criado no curso de imersão front-end da alura.
 
 ![](./assets/Captura%20de%20tela%20de%202025-01-28%2019-54-26.png)
--- script.js
+++ script.js
@@ -5,4 +5,50 @@ document.addEventListener("DOMContentLoaded", function () {
     menuToggle.addEventListener("click", function () {
         navMenu.classList.toggle("active"); // Alterna a classe 'active'
     });
+
+    const inputBusca = document.querySelector("#busca");
+
+    inputBusca.addEventListener("input", (e) => {
+        const entrada = e.target.value;
+
+        const divResultados = document.querySelector("#resultados");
+        divResultados.innerHTML = "";
+
+        if(entrada != ""){
+            fetch("./api.json")
+            .then(resultado => resultado.json())
+            .then(titulos => {
+                const resultados = titulos.filter(titulo => includesCaseInsensitive(titulo.nome, entrada) || includesCaseInsensitive(titulo.artista, entrada));
+                mostraResultadosBusca(resultados);
+            })
+            .catch(error => console.error(error))
+        }
+
+
+    });
+
+    inputBusca.addEventListener("blur", escodeResultadosBusca);
 });
+
+function mostraResultadosBusca(resultados){
+    const ul = document.createElement("ul");
+
+    const lis = resultados.forEach(titulo => ul.insertAdjacentHTML('beforeend', "<li>" +
+        `<img src='${titulo.capa}' width="40">` +
+        `<span class="titulo-nome">${titulo.nome}</span>` +
+        `<span>${titulo.artista}</span>` +
+        `</li>`
+    ));
+    
+    const divResultados = document.querySelector("#resultados");
+    divResultados.classList.remove("invisivel");
+    divResultados.appendChild(ul);
+}
+
+function escodeResultadosBusca(){
+    const divResultados = document.querySelector("#resultados");
+    divResultados.classList.add("invisivel");
+}
+
+const includesCaseInsensitive = (str, searchString) =>
+    new RegExp(searchString, 'i').test(str);
